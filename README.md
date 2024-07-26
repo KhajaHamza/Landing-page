2946c8bf992dfd8a6d47b87d23c93878a4e9a37b
Syed Shoaib <shoaib.ahmed@aidenai.com>
2024-07-26 23:46:25 +0530

    got all files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Landing-page
+# Landing-page
+# Landing-page
+# Landing-page
+# Landing-page
--- pdf-html.js
+++
@@ -1,8 +0,0 @@
-const pdf2html = require('pdf2html');
-
-async function convertPdfToHtml() {
-    const html = await pdf2html.html('Hospital Bill.pdf');
-    console.log(html);
-}
-
-convertPdfToHtml();
